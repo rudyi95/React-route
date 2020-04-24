ad696f32019dc58f6d9d571fb00eb6be1c100f80
rudyi95 <rud11@meta.ua>
2020-04-24 13:35:20 +0300

    Updates

---
+++ precache-manifest.8b2edebbf0942233180040b86a3a1239.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69476df1725547fd05f41f665810532a",
+    "url": "/React-route/index.html"
+  },
+  {
+    "revision": "c7ae00e82c23b43eadb8",
+    "url": "/React-route/static/css/main.f2505a11.chunk.css"
+  },
+  {
+    "revision": "864137ddc550881cfb37",
+    "url": "/React-route/static/js/2.99befb3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-route/static/js/2.99befb3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ae00e82c23b43eadb8",
+    "url": "/React-route/static/js/main.9549096a.chunk.js"
+  },
+  {
+    "revision": "a24e4f90cf1e53b0b6df",
+    "url": "/React-route/static/js/runtime-main.34ecdcf4.js"
+  },
+  {
+    "revision": "109a55380c4eed8232c21f831cc8c4ce",
+    "url": "/React-route/static/media/Belvedere.109a5538.jpg"
+  },
+  {
+    "revision": "efc28275568c768bca0303c6d7da968b",
+    "url": "/React-route/static/media/Ketel One.efc28275.png"
+  },
+  {
+    "revision": "9612dd4b7e2cc0c46f8239b19ff6e72d",
+    "url": "/React-route/static/media/Nazar.9612dd4b.jpg"
+  },
+  {
+    "revision": "777c5f103c371d75477d1ccc064b56a5",
+    "url": "/React-route/static/media/RADA Premium Classic.777c5f10.jpg"
+  },
+  {
+    "revision": "a4e457b0b87fe3809b255187b0da688f",
+    "url": "/React-route/static/media/Tanqueray  Flor de Sevilla Gin.a4e457b0.jpg"
+  },
+  {
+    "revision": "546beb8bed46e4248b96a43bf8917adb",
+    "url": "/React-route/static/media/Tanqueray №TEN.546beb8b.jpg"
+  },
+  {
+    "revision": "05ba51478fd7b7e5d1d542ca7d8d2526",
+    "url": "/React-route/static/media/The Botanist.05ba5147.jpg"
+  },
+  {
+    "revision": "69e17f0b2939268528c23cfe1404bba6",
+    "url": "/React-route/static/media/alcohol-background.69e17f0b.jpg"
+  },
+  {
+    "revision": "989c1e81db6fae95668248d699b663ee",
+    "url": "/React-route/static/media/iphone.989c1e81.png"
+  }
+]);
